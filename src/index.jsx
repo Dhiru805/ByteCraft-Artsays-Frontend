fbc5f246c80951c312faf1682f8aed668fc11a54
Dhiraj Zope <dhiraj.zope1997@gmail.com>
2026-03-24 13:42:05 +0530

    minor fix in index.js

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 // import './index.css'; 
 
 import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3001";
+
+function Root() {
+  const [googleClientId, setGoogleClientId] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch(`${API_URL}/api/get-google-settings`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data?.googleClientId) {
+          setGoogleClientId(data.googleClientId);
+        }
+      })
+      .catch(() => {})
+      .finally(() => setLoaded(true));
+  }, []);
+
+  if (!loaded) return null;
+
+  return (
+    <GoogleOAuthProvider clientId={googleClientId || ""}>
+      <App />
+    </GoogleOAuthProvider>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-      <App />
-    </GoogleOAuthProvider>
+    <Root />
   </React.StrictMode>
 );
 
